0653e3d42d8f7114bef6a7d94236218b72a43268
Cynthia Smith and Will Swanson <will@>
2018-06-05 11:38:48 -0700

    add logic to convert input to puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,27 @@ $(document).ready(function() {
     var processing = $("input#puzzleText").val();
     $("#puzzleBody").show();
     $("#firstSection").hide();
-    alert("Answer this question: " + processing);
-
+    // alert("Answer this question: " + processing);
+    // split input string into array
+    // foreach on array to push to new array
+    // if vowel, push "-" instead
+    // otherwise push consonant
+    // dispaly new array as string on html
+    var procArray = processing.split("");
+    var outArray =[];
+    function transform(param1, param2){
+      param1.forEach(function(step) {
+        if (step.toString().toLowerCase() === "a") {
+          param2.push("*")
+        } else {
+          param2.push(step)
+        }
+      });
+      return param2;
+    };
+    var bob = transform(procArray,outArray);
+    var fred = bob.join("");
+    alert(fred);
     event.preventDefault();
 
   });
